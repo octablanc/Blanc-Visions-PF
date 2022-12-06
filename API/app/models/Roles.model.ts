8aadf49bb726b5d353c97aacb3c2d3e0d73e0c37
Tomas Flores <tomy24ssf@gmail.com>
2022-12-06 16:34:10 -0300

    relos refactory

--- API/app/models/Roles.model.ts
+++ API/app/models/Roles.model.ts
@@ -1,14 +1,18 @@
-import { DataTypes, Sequelize } from 'sequelize';
+import { DataTypes, Sequelize } from "sequelize";
 
 export default (sequelize: Sequelize) => {
-    sequelize.define('roles', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        state: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    }, { freezeTableName: true });
-}
+  sequelize.define(
+    "roles",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { freezeTableName: true }
+  );
+};
