8a8d13d1c69344354007cc0d4d89817289f016c8
Tomas Flores <tomy24ssf@gmail.com>
2022-12-06 20:59:37 -0300

    add routes and controllers

--- API/app/controllers/ExampleController.ts
+++ API/app/controllers/ExampleController.ts
@@ -1,10 +1,18 @@
-import { Request, Response} from 'express';
+import { Request, Response } from "express";
+// import DBcontext from "../../config/ConnectionDB";
 
-export function getUsers( _req:Request, res:Response){
-    try {
-        return res.send([{message: "all users"}]);
-    } catch ({message}) {
-        return res.status(400).send({message});
-    }
+export function getUsers(_req: Request, res: Response) {
+  try {
+    return res.json({ message: "all users", arr: [] });
+  } catch ({ message }) {
+    return res.status(400).json({ message });
+  }
 }
 
+export function postUser(_req: Request, res: Response) {
+  try {
+    return res.send([{ message: "POST USERRR  users" }]);
+  } catch ({ message }) {
+    return res.status(400).json({ message });
+  }
+}
--- API/app/routes/products.ts
+++ API/app/routes/products.ts
@@ -1,15 +1,10 @@
-import express from 'express';
-import { getUsers, postUser } from '../controllers/ExampleController';
+import express from "express";
+import { getProducts, postUser } from "../controllers/ProductsControllers";
 
 const router = express.Router();
-router.get('/', getUsers);
+
+router.get("/", getProducts);
 router.post('/', postUser);
 
 // Each router will have to be exported with "module.exports" because the loader uses "require" to import each router.
 module.exports = router;
-
-// /product
-// /users
-// /cart
-// /orden De compras (?)
-// /categorias (?)
