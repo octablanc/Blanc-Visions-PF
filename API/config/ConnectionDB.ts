c5f4c6660e8f2f1d129f4f6bd535f7a51d09c9d5
Lucas Alegre <lucasealegre123@gmail.com>
2022-12-06 17:19:07 -0300

    models relation terminadas

--- API/config/ConnectionDB.ts
+++ API/config/ConnectionDB.ts
@@ -1,72 +1,68 @@
-import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import * as dotenv from "dotenv";
 
 //Models
-import Categories from '../app/models/Categories.model';
-import Products from '../app/models/Products.model';
-import Roles from '../app/models/Roles.model';
-import Sales_Detail from '../app/models/CartSale.model';
-import Sales from '../app/models/OrderBuy';
-import Users from '../app/models/Users.model';
+import CartBuy from "../app/models/CartBuy.model";
+import CartSale from "../app/models/CartSale.model";
+import Categories from "../app/models/Categories.model";
+import OrderBuy from "../app/models/OrderBuy.model";
+import Products from "../app/models/Products.model";
+import Roles from "../app/models/Roles.model";
+import Users from "../app/models/Users.model";
 
 // Creates connection to the data base with Sequelize or MongoDB.
 dotenv.config();
-const {
-    DB_URL
-} = process.env;
+const { DB_URL } = process.env;
 
 const DBcontext = new Sequelize(`${DB_URL}`, {
-    logging: false,
-    native: false
+  logging: false,
+  native: false,
 });
 
 // Creating tables from models
+CartBuy(DBcontext);
+CartSale(DBcontext);
 Categories(DBcontext);
+OrderBuy(DBcontext);
 Products(DBcontext);
 Roles(DBcontext);
-Sales_Detail(DBcontext);
-Sales(DBcontext);
 Users(DBcontext);
 
-const { users, users_addresses, sales, roles, products, categories, sales_detail } = DBcontext.models;
+const { cart, cartSale, categories, orderSales, products, roles, users } =
+  DBcontext.models;
 
-// A user can have a single role, roles can have many users (one to many).
-roles.hasMany(users)
-users.belongsTo(roles);
 /*
 Un Usuario puede tener solo 1 Rol. Si el rol es usuario entonces tendra un carrito de compra. Si el Rol es Admin, entonces tendra un carrito de ventas.
-Tanto el carrito de compra como el de venta, tendran acceso a laOrden de Compra.
-La orden tiene muchos productos, y los productos corresponden a una categoria.
-
-
+Tanto el carrito de compra como el de venta, tendran acceso a la Orden de Compra.
+La Orden de compra tiene muchos productos, y los productos corresponden a una categoria.
 */
 
+// A user can have a single role, roles can have many users (one to many).
+roles.hasMany(users);
+users.belongsTo(roles);
 
-// A product can have only one category, one category many products (one to many).
+//A user can have a single carrusel, carrusel can have a single users.(one to one)
+users.hasOne(cart);
+cart.belongsTo(users);
+
+//A user can have a single carrusel of the Venta. Carrusel can have a single users.(one to one)
+users.hasOne(cartSale);
+cartSale.belongsTo(users);
+
+//A Carrusel can have a single Order of Compra. Order of Compra can have a single Carrusel.(one to one)
+cart.hasOne(orderSales);
+orderSales.belongsTo(cart);
+
+//A Carrusel of Venta can have a single Order of Compra. Order of Compra can have a single Carrusel of Venta.(one to one)
+cartSale.hasOne(orderSales);
+orderSales.belongsTo(cartSale);
+
+//A Order of Compra can have many Products. Products can have many Order of Compra.(many to many)
+orderSales.belongsToMany(products, { through: "orderSales_Products" });
+products.belongsToMany(orderSales, { through: "orderSales_Products" });
+
+//One category many products. A product can have only one category (one to many).
 categories.hasMany(products);
 products.belongsTo(categories);
-
-// A product has many sales details, one sales detail corresponds to one product (one to many).
-products.hasMany(sales_detail);
-sales_detail.belongsTo(products);
-
-// A User has one address, an address has only one user (One to One).
-users_addresses.hasOne(users);
-users.belongsTo(users_addresses);
-
-
-
-// A sale has only one address, one address can correspond to several sales (one to many).
-users_addresses.hasMany(sales, { foreignKey: 'deliveryAddress'});
-// sales.belongsTo(users_addresses); <-- this will create another foreign key that we do not need. 
-
-// A sale has many sale details, a sale detail corresponds to a sale (one to many).
-sales.hasMany(sales_detail);
-sales_detail.belongsTo(sales);
-
-// One sale corresponds to one user, one user can have many sales (one to many).
-users.hasMany(sales);
-sales.belongsTo(users);
-users.hasMany(sales, { foreignKey: 'userBuyerId' }); // Buyer relation
 
 export default DBcontext;
