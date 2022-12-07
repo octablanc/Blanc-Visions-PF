8af1bc5939a76c1761d5d136aece4fcadb65d8b0
Tomas Flores <tomy24ssf@gmail.com>
2022-12-07 16:22:15 -0300

    refactory controllers cart

--- API/app/controllers/CartControllers.ts
+++ API/app/controllers/CartControllers.ts
@@ -1,39 +1,27 @@
 import { Request, Response } from "express";
 import DBcontext from "../../config/ConnectionDB";
-// import UsersModel from "../models/Users.model";
 const { cartBuy, users } = DBcontext.models;
-// const { cartBuy } = DBcontext.models;
 
 export async function getCart(_req: Request, res: Response) {
   try {
-    // const carts = await cartBuy.findByPk(1);
-    
-    // const carts = await cartBuy.findAll({attributes:['priceTotal'], include:users});
-      const carts = await cartBuy.findAll({ include: users });
-
+    const carts = await cartBuy.findAll({ include: users });
     return res.json({ message: "Get Carts", carts });
   } catch ({ message }) {
     return res.status(400).send({ message });
   }
 }
 
-// const actividadCreate = await Turistas.create(req.body);
-// console.log(actividadCreate.__proto__);
 export async function postCart(req: Request, res: Response) {
   try {
     const { priceTotal, userId } = req.body;
-    const cart = await cartBuy.create({ priceTotal });
-    // const cart = await cartBuy.create(req.body);
+    // Tambien tiene que llegar un "id orden de compra"
+    const cartCreate = await cartBuy.create({ priceTotal });
     
-      // await cart.adduserId(userId)
-      const idEncontrado = await users.findByPk(userId)
-      await cart.addUser(idEncontrado)
-      // userId
-    // console.log("CREATE 1 =>", cartUno);
-    // console.log("CREATE 2 =>", cartDos);
-    // console.log("CREATE 3 =>", cartTres);
+    const idEncontrado = await users.findByPk(userId);
+    // await cartCreate.adduserId(idEncontrado) // falta refactorizar esta linea
+    console.log("busca ID => ", idEncontrado);
 
-    return res.send({ message: "Post cart", Create: cart });
+    return res.send({ message: "Post cart", Create: cartCreate });
   } catch ({ message }) {
     console.log("MSG ERR=> ", message);
     return res.status(400).send({ message });
--- API/app/controllers/ExampleController2.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response} from 'express';
-
-export function getUsers( _req:Request, res:Response){
-    try {
-        return res.send([{message: "all users x 2"}]);
-    } catch ({message}) {
-        return res.status(400).send({message});
-    }
-}
