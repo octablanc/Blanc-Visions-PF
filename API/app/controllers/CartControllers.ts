ee01feabfd34c752eb33983a2d3a32b4b830b4bb
Tomas Flores <tomy24ssf@gmail.com>
2022-12-07 15:35:19 -0300

    refactory cartControllers

--- API/app/controllers/CartControllers.ts
+++ API/app/controllers/CartControllers.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express";
 import DBcontext from "../../config/ConnectionDB";
-const { cartBuy } = DBcontext.models;
+// import UsersModel from "../models/Users.model";
+const { cartBuy, users } = DBcontext.models;
+// const { cartBuy } = DBcontext.models;
 
-export async function getCart(req: Request, res: Response) {
-  const { id } = req.body;
+export async function getCart(_req: Request, res: Response) {
   try {
-    console.log(id);
-
-    //   const carts = await cartBuy.findByPk(id, { include: Users });
-    const carts = await cartBuy.findAll();
-    //   const carts = await cartBuy.findAll({ include: users });
+    // const carts = await cartBuy.findByPk(1);
+    
+    // const carts = await cartBuy.findAll({attributes:['priceTotal'], include:users});
+      const carts = await cartBuy.findAll({ include: users });
 
     return res.json({ message: "Get Carts", carts });
   } catch ({ message }) {
@@ -21,25 +21,19 @@ export async function getCart(req: Request, res: Response) {
 // console.log(actividadCreate.__proto__);
 export async function postCart(req: Request, res: Response) {
   try {
-    const { priceTotal } = req.body;
-    console.log(priceTotal);
-
-    const cart1 = { priceTotal: 10 };
-    // const cart2 = { priceTotal: 20 };
-    // const cart3 = { priceTotal: 30 };
-
-    // const cartUno = await cartBuy.create(cart1);
-    // const cartDos = await cartBuy.create(cart2);
-    //   const cartTres = await cartBuy.create(cart3);
-    await cartBuy.create(cart1);
-    // await cartBuy.create(cart2);
-    // await cartBuy.create(cart3);
-
+    const { priceTotal, userId } = req.body;
+    const cart = await cartBuy.create({ priceTotal });
+    // const cart = await cartBuy.create(req.body);
+    
+      // await cart.adduserId(userId)
+      const idEncontrado = await users.findByPk(userId)
+      await cart.addUser(idEncontrado)
+      // userId
     // console.log("CREATE 1 =>", cartUno);
     // console.log("CREATE 2 =>", cartDos);
     // console.log("CREATE 3 =>", cartTres);
 
-    return res.send({ message: "Post cart", msg: "Creados 3 carts" });
+    return res.send({ message: "Post cart", Create: cart });
   } catch ({ message }) {
     console.log("MSG ERR=> ", message);
     return res.status(400).send({ message });
