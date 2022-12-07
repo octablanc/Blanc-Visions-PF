74fde3cc847279a4a8bdcd79b76769e4029b386f
Tomas Flores <tomy24ssf@gmail.com>
2022-12-07 16:37:37 -0300

    refatory cartControllers

--- API/app/controllers/CartControllers.ts
+++ API/app/controllers/CartControllers.ts
@@ -5,7 +5,7 @@ const { cartBuy, users } = DBcontext.models;
 export async function getCart(req: Request, res: Response) {
   const { id } = req.body;
   try {
-    const cart = await cartBuy.findByPk(id,{ include: users });
+    const cart = await cartBuy.findByPk(id);
     return res.json({ message: "Get Carts", cart });
   } catch ({ message }) {
     return res.status(400).send({ message });
