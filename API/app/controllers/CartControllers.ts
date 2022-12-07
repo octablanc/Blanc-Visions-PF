e379c293fee979454ee5ea94d96270cdc7938beb
Tomas Flores <tomy24ssf@gmail.com>
2022-12-07 13:08:29 -0300

    refactory cartControlller

--- API/app/controllers/CartControllers.ts
+++ API/app/controllers/CartControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import DBcontext from "../../config/ConnectionDB";
-const { cartBuy, Users } = DBcontext.models;
+const { cartBuy } = DBcontext.models;
 
 export async function getCart(req: Request, res: Response) {
   const { id } = req.body;
@@ -8,35 +8,40 @@ export async function getCart(req: Request, res: Response) {
     console.log(id);
 
     //   const carts = await cartBuy.findByPk(id, { include: Users });
-    const carts = await cartBuy.findAll({ include: Users });
+    const carts = await cartBuy.findAll();
+    //   const carts = await cartBuy.findAll({ include: users });
+
     return res.json({ message: "Get Carts", carts });
   } catch ({ message }) {
     return res.status(400).send({ message });
   }
 }
 
+// const actividadCreate = await Turistas.create(req.body);
+// console.log(actividadCreate.__proto__);
 export async function postCart(req: Request, res: Response) {
   try {
     const { priceTotal } = req.body;
+    console.log(priceTotal);
 
-      const cart = await cartBuy.create({ priceTotal });
-    console.log(cart);
-    
-    //   Relacion
-    //   console.log(cart.__proto__);
-      
-    // const actividadCreate = await Turistas.create(req.body);
-    // console.log(actividadCreate.__proto__);
-      
-    //   const cart = await cartBuy.create({ priceTotal: priceTotal2 });
-    //   console.log(cart);
-    //   console.log(cart.__proto__);
+    const cart1 = { priceTotal: 10 };
+    // const cart2 = { priceTotal: 20 };
+    // const cart3 = { priceTotal: 30 };
 
+    // const cartUno = await cartBuy.create(cart1);
+    // const cartDos = await cartBuy.create(cart2);
+    //   const cartTres = await cartBuy.create(cart3);
+    await cartBuy.create(cart1);
+    // await cartBuy.create(cart2);
+    // await cartBuy.create(cart3);
 
-    // await cart.();
+    // console.log("CREATE 1 =>", cartUno);
+    // console.log("CREATE 2 =>", cartDos);
+    // console.log("CREATE 3 =>", cartTres);
 
-    return res.send({ message: "Post cart", priceTotal });
+    return res.send({ message: "Post cart", msg: "Creados 3 carts" });
   } catch ({ message }) {
+    console.log("MSG ERR=> ", message);
     return res.status(400).send({ message });
   }
 }
