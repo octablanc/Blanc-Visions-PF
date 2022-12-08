416bac5c8cb8171cffc32f0081abda17f01635f3
Lucas Alegre <lucasealegre123@gmail.com>
2022-12-08 15:09:17 -0300

    Prueba

--- API/app/routes/users.ts
+++ API/app/routes/users.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/', getUsers);
 router.post('/', postUser);
 
+
 // Each router will have to be exported with "module.exports" because the loader uses "require" to import each router.
 module.exports = router;
