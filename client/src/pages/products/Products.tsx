14e8df0d53bab580e9746e8a410f74599c9121d5
Florencia <florcaro621@gmail.com>
2022-12-10 16:57:26 -0300

    getProducts with reduxToolk

--- client/src/pages/products/Products.tsx
+++ client/src/pages/products/Products.tsx
@@ -1,77 +1,33 @@
+import { useEffect } from 'react';
+// styles
+import { Conteiner } from './styled-components/styled';
+//components
 import { Filters } from '../../components/Filters';
-import './styled-components/prod.css';
-import { Conteiner, Card, Div, Div2 } from './styled-components/styled';
-import { useEffect, useState } from 'react';
-import Pro from './models/ejProducts';
-import { AiOutlineStar, AiFillStar } from 'react-icons/ai';
-import { BsCart4 } from 'react-icons/bs';
+//redux
+import { useAppDispatch, useAppSelector } from '../../redux/app/hooks';
+import { getProducts } from '../../redux/slices/products';
+import { ProductItem } from './components/ProductItem';
 
 export const Products = () => {
-  const [product, setProduct] = useState<Pro[]>([]);
+  const dispatch = useAppDispatch();
+  const { products } = useAppSelector((state) => state.productsState);
 
   useEffect(() => {
-    fetch(`http://localhost:4000/products`)
-      .then((resp) => resp.json())
-      .then((prod) => {
-        console.log(prod);
-        setProduct(prod);
-      });
-  }, []);
+    dispatch(getProducts());
+  }, [dispatch]);
 
   return (
     <Conteiner className='container'>
-      <Div>
-        <Filters />
-      </Div>
-      <Div2>
-        <h1 className='cat'>Name Categoria</h1>
-        <div className='contieneTodo'>
-          {product.length &&
-            product.map((e) => (
-              <Card>
-                <div className='image'>
-                  <img src={e.image} alt='notfound' />
-                </div>
-                <div className='description'>
-                  <h3>{e.name}</h3>
-                  <div>
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiFillStar />
-                    <AiOutlineStar />
-                    <button>
-                      <BsCart4 />
-                    </button>
-                  </div>
-                  <ul>
-                    {e.description.map((e) => (
-                      <li>{e}</li>
-                    ))}
-                  </ul>
-                  <h3>${e.price}</h3>
-                </div>
-              </Card>
+      <Filters />
+      <div>
+        <h1 className='text-center'>Name Categoria</h1>
+        <div>
+          {products.length &&
+            products.map((product) => (
+              <ProductItem key={product.code} product={product} />
             ))}
         </div>
-      </Div2>
+      </div>
     </Conteiner>
   );
 };
-
-// const Div = styled.div`
-//   background: red;
-// `;
-// const Div2 = styled.div`
-//   background: green;
-// `;
-// const Div3 = styled.div`
-//   background: blue;
-// `;
-// const Conteiner = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   background-color: grey;
-//   height: 20rem;
-//   align-items: flex-send;
-// `; displayflexxxx
--- client/src/pages/products/models/ejProducts.tsx
+++ client/src/pages/products/models/ejProducts.tsx
@@ -1,4 +1,4 @@
-export default interface Pro {
+export interface Pro {
   map(arg0: (item: any) => JSX.Element): import('react').ReactNode;
   name: string;
   code: number;
@@ -13,49 +13,7 @@ export default interface Pro {
   features: string[];
 }
 
-export default interface Categ {
+export interface Categ {
   name: string;
   value: string;
 }
-
-// let camaras: Ejemplo[] = [
-//   {
-//     nameProduct: 'Canon EOS Rebel Kit T7 + lente 18-55mm IS II DSLR',
-//     atributes: [
-//       'Sensor CMOS APS-C de 32.5M',
-//       'Procesador de imagen DIGIC 8',
-//       'Grabación de video UHD 4K30p y Full HD 120',
-//       'Pantalla LCD táctil de ángulo variable de 3 y 1.04 m de punto',
-//     ],
-//     cuotes: 3,
-//     precio: 144.279,
-//     image: img,
-//     entrega: '24 de diciembre',
-//   },
-//   {
-//     nameProduct: 'CANON EOS 5D MARK IV BODY IMPORTACION OFICIAL',
-//     atributes: [
-//       'Sensor CMOS APS-C de 32.5M',
-//       'Procesador de imagen DIGIC 8',
-//       'Grabación de video UHD 4K30p y Full HD 120',
-//       'Pantalla LCD táctil de ángulo variable de 3 y 1.04 m de punto',
-//     ],
-//     cuotes: 6,
-//     image: img,
-//     precio: 281.699,
-//     entrega: '8 marzo',
-//   },
-//   {
-//     nameProduct: 'CANON EOS 6D MARK II DSLR BODY',
-//     atributes: [
-//       'Sensor CMOS APS-C de 32.5M',
-//       'Procesador de imagen DIGIC 8',
-//       'Grabación de video UHD 4K30p y Full HD 120',
-//       'Pantalla LCD táctil de ángulo variable de 3 y 1.04 m de punto',
-//     ],
-//     cuotes: 5,
-//     image: img,
-//     precio: 944.19,
-//     entrega: '3 de abril',
-//   },
-// ];
