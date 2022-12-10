f2e7db68b93e4020cc5ce7241d253051e43fb506
Tomas Flores <tomy24ssf@gmail.com>
2022-12-10 19:22:58 -0300

    adding route users.delete

--- API/app/routes/users.ts
+++ API/app/routes/users.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getUsers, postUser } from '../controllers/UsersController';
+import { getUsers, postUser, deleteUser } from '../controllers/UsersController';
 
 const router = express.Router();
 router.get('/', getUsers);
 router.post('/', postUser);
+router.delete('/', deleteUser);
+
 
 // Each router will have to be exported with "module.exports" because the loader uses "require" to import each router.
 module.exports = router;
