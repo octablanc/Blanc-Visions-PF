d463dc732f9ac5b0b53fea213a0d6f546ec0a814
Lucas Alegre <lucasealegre123@gmail.com>
2022-12-10 14:25:17 -0300

    Adding components Erroor to 404

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Detail } from './pages/detail/Detail';
-import { Home } from './pages/home/Home';
-import { Products } from './pages/products/Products';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Detail } from "./pages/detail/Detail";
+import { Home } from "./pages/home/Home";
+import { Products } from "./pages/products/Products";
+import { Error } from "./pages/error/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index path='home' element={<Home />} />
-          <Route path='products' element={<Products />} />
-          <Route path='detail' element={<Detail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index path="home" element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="detail" element={<Detail />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
