853ee1a061a426602975b8ec4fd97bea4c615427
Tomas Flores <tomy24ssf@gmail.com>
2022-12-12 18:28:07 -0300

    imagesControllers: adding method POST GET DELETE images

---
+++ API/app/controllers/ImagesController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { images } from "../../config/ConnectionDB";
+
+// Model
+/*
+{
+  url_image: "https://http2.mlstatic.com/D_NQ_NP_979408-MLA42843661201_072020-O.webp",
+  state: true,
+  productId: 1,
+}*/
+
+export async function postImages(req: Request, res: Response) {
+  try {
+    const imageCreate = await images.create(req.body);
+    return res.json({ message: "Imagenes agregadas.", imageCreate });
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(400).send({ message });
+  }
+}
+
+export async function deleteImages(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    await images.destroy({ where: { id } });
+    return res.json({ message: "images has been discharged!" });
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(400).send({ message });
+  }
+}
+
+export async function getImageByPk(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const imageByPk = await images.findByPk(id);
+    return res.json(imageByPk);
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(400).send({ message });
+  }
+}
