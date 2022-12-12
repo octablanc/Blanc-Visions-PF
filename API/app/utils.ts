624fc348282c50e1360966cef0f8e8a2d8b81136
Tomas Flores <tomy24ssf@gmail.com>
2022-12-12 20:40:24 -0300

    adding products array

--- API/app/utils.ts
+++ API/app/utils.ts
@@ -7,156 +7,287 @@
 //   "state": true,
 //   "categoryId": 1
 //  }
-
 export const category = [
   {
-    name: 'Camaras y Lentes',
-    description: 'lorem ipsum',
+    name: "Camaras y Lentes",
+    description: "lorem ipsum",
     state: true,
   },
   {
-    name: 'Accesorios',
-    description: 'lorem ipsum',
+    name: "Accesorios",
+    description: "lorem ipsum",
     state: true,
   },
   {
-    name: 'Flashes e Iluminacion',
-    description: 'lorem ipsum',
+    name: "Flashes e Iluminacion",
+    description: "lorem ipsum",
     state: true,
   },
   {
-    name: 'Tripodes y Soportes',
-    description: 'lorem ipsum',
+    name: "Tripodes y Soportes",
+    description: "lorem ipsum",
     state: true,
   },
   {
-    name: 'Audio y Video',
-    description: 'lorem ipsum',
+    name: "Audio y Video",
+    description: "lorem ipsum",
     state: true,
   },
 ];
-
+// Camaras y Lentes  1cea
+// Accesorios    2
+// Flashes e Iluminacion  3
+// Tripodes y Soportes  4
+// Audio y Video  5
 export const data = [
   {
-    code: 'F0021',
-    name: 'sony alfha',
+    code: "F0021",
+    name: "sony alfha",
     price: 10000,
     stock: 50,
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
     image:
-      'https://http2.mlstatic.com/D_NQ_NP_889577-MLA41501060479_042020-O.webp',
+      "https://http2.mlstatic.com/D_NQ_NP_889577-MLA41501060479_042020-O.webp",
     state: true,
     categoryId: 1,
   },
   {
-    code: 'F0022',
-    name: 'Drone JJRC JJPRO Epik',
-    price: 30000,
+    code: "F027",
+    name: "anon EOS Rebel Kit T7 + lente 18-55mm IS II DSLR color negro",
+    price: 15000,
+    stock: 30,
+    description: ` 
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_822322-MLA48551234826_122021-O.webp",
+    state: true,
+    categoryId: 1,
+  },
+  {
+    code: "Peak",
+    name: "Peak Design Tripode para viaje de aluminio CB-5-150-AL",
+    price: 15000,
+    stock: 2,
+    description: ` 
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/2182_1_z.jpg",
+    state: true,
+    categoryId: 4,
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=aXU5dlNlem5lU2VtckIvSFNHdktUaUhabFMvMW9pY1BUQ1MyWnBvVGJwYz0
+  },
+  {
+    code: "Pakd",
+    name: "Vanguard VESTA 204AP",
+    price: 18500,
+    stock: 7,
+    description: ` 
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/1751_1_z.jpg",
+    state: true,
+    categoryId: 4,
+    //https://www.digi-zoom.com.ar/productos_detalle.php?i=VU5vRGNYVEpOQmZZellSb011d3pPNlluMFdSRURpNVNFNEVxTDZLVVc3cz0
+  },
+  {
+    code: "VGSO",
+    name: "Vanguard VK 203 APM",
+    price: 9700,
     stock: 5,
     description: ` 
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
     `,
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_681300-MLA47916761205_102021-O.webp',
+
+    image: "https://www.digi-zoom.com.ar/images/productos/1488_1_z.jpg",
+    state: true,
+    categoryId: 4,
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=NjR3dGRIZTY1K05welpBSWhUS1RQNWdPWmZtd0ZObldIVkR0ckdkSU9kRT0
+  },
+  {
+    code: "Pe4k",
+    name: "Vanguard VEO 2 235 AB BLUE",
+    price: 9700,
+    stock: 1,
+    description: ` 
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/1488_1_z.jpg",
+    state: true,
+    categoryId: 4,
+    //https://www.digi-zoom.com.ar/productos_detalle.php?i=Wlp6T0pqT04xN1B0bWd4SThESlZGZVVXT2VuWmpsQ1ViMnliRXFuZXNtMD0
+  },
+  {
+    code: "db00",
+    name: "Nikon SB-700 Flash",
+    price: 100200,
+    stock: 17,
+    description: ` 
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/300_1_z.jpg",
+    state: true,
+    categoryId: 3,
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=N2dkSzVpWU5halo0RmJrNmFhUzdDRmdEMFZwRmtIWXNqc0kzRUNRTGRhcz0
+  },
+  {
+    code: "MF12",
+    name: "Godox MF12 Macro Flash",
+    price: 43776,
+    stock: 6,
+    description: ` 
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/2283_1_z.jpg",
+    state: true,
+    categoryId: 3,
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=cFZyb0lGbnUrNTA0MHFEVUo5TjZlTFFFTGNtQ2dWN3EyTk41MW5NM1B3MD0
+  },
+  {
+    code: "Fuji",
+    name: "Godox V1 Flash para Fuji",
+    price: 43776,
+    stock: 6,
+    description: ` 
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/1916_1_z.jpg",
+    state: true,
+    categoryId: 3,
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=WDhjSUo2UzNiMTZzSUFDMy8zYkw2aGQ3amJnWHh1a3R4d2EvYUl5ZVpPaz0
+  },
+  {
+    code: "goro",
+    name: "Go Pro Soporte para tabla de bodyboard",
+    price: 11000,
+    stock: 6,
+    description: ` 
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/640_1_z.jpg",
+    state: true,
+    categoryId: 2,
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=NyttZzZYdFNTNnNVdXZXbXIzMFJLVWxQczVCQzF4SDNjbzVlc3FGM0FITT0
+  },
+  {
+    code: "gorns",
+    name: "Go Pro Arnes para el pecho",
+    price: 11000,
+    stock: 6,
+    description: ` 
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/640_1_z.jpg",
     state: true,
     categoryId: 2,
   },
   {
-    code: 'F0023',
-    name: 'Gafas De Sol Rectangular Mujer Anteojos Moda Lentes Vintage',
-    price: 5000,
-    stock: 30,
+    code: "reel",
+    name: "CANON EOS REBEL T7 / 1500D (18-55 MM) IMPORTACION PROPIA",
+    price: 4999,
+    stock: 6,
     description: ` 
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
     `,
 
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_840319-MLA51390583982_092022-O.webp',
-    state: true,
-    categoryId: 2,
-  },
-  {
-    code: 'F0024',
-    name: 'Catalejo Infantil F300x30ca Galileo',
-    price: 50000,
-    stock: 30,
-    description: ` 
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-    `,
-
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_641189-MLA44133613502_112020-O.webp',
+    image: "https://www.digi-zoom.com.ar/images/productos/2181_2_z.jpg",
     state: true,
     categoryId: 1,
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=aEY5Z2RrZE84emtZK1NZckt3azZlTUpqNmJhRnE3VmRudHJTUmF2Q2dMOD0
   },
   {
-    code: 'F0025',
-    name: 'Portaretrato Cuadro Foto 20x30 Diploma Para Apoyar O Colgar',
-    price: 10000,
-    stock: 30,
+    code: "na9d",
+    name: "Canon EOS 90D Body Importacion Oficial",
+    price: 4999,
+    stock: 6,
     description: ` 
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
     `,
 
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_988342-MLA50714009243_072022-O.webp',
+    image: "https://www.digi-zoom.com.ar/images/productos/2199_1_z.jpg",
     state: true,
-    categoryId: 2,
+    categoryId: 1,  
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=V0cvcDg2MFNjbXBlZXNkck5VSjlSYUF1T1dmRmg5cEFaZUg0UVF3YXVVQT0
   },
   {
-    code: 'F0026',
-    name: 'Álbum De Fotos Nuestro Libro De Aventuras Up',
-    price: 15000,
-    stock: 30,
-    description: ` 
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    code: "na9fd",
+    name: "Canon EOS 5D Mark IV (24-105 mm IS USM) Importacion Oficial",
+    price: 4999,
+    stock: 6,
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an  of type and scrambled it to make a type specimen book.
     `,
 
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_818903-MLA48281123339_112021-O.webp',
+    image: "https://www.digi-zoom.com.ar/images/productos/2197_1_z.jpg",
     state: true,
-    categoryId: 2,
+    categoryId: 1,  
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=V0cvcDg2MFNjbXBlZXNkck5VSjlSYUF1T1dmRmg5cEFaZUg0UVF3YXVVQT0
   },
   {
-    code: 'F0027',
-    name: 'anon EOS Rebel Kit T7 + lente 18-55mm IS II DSLR color negro',
-    price: 15000,
-    stock: 30,
+    code: "SNCO",
+    name: "SYNCO MICRÓFONO LAVALIER CON CABLE SY-S6R-LAV",
+    price: 4999,
+    stock: 6,
     description: ` 
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
     `,
 
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_822322-MLA48551234826_122021-O.webp',
+    image: "https://www.digi-zoom.com.ar/images/productos/2069_1_z.jpg",
     state: true,
-    categoryId: 1,
+    categoryId: 5, 
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=U0kwVTRQR2hXV2NQVWd5bDgvVlY5SHU3WVcxUkhCOTRnbThQd0dScWw1Yz0
+
   },
   {
-    code: 'F0028',
-    name: 'drone Daming DM107S con cámara FullHD black 1 batería',
-    price: 125000,
-    stock: 2,
+    code: "SYe1",
+    name: "Synco Micrófono de sonido en vivo SY-E10-MIC",
+    price: 4999,
+    stock: 6,
     description: ` 
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
     `,
 
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_855963-MLA40373328806_012020-O.webp',
+    image: "https://www.digi-zoom.com.ar/images/productos/2066_1_z.jpg",
     state: true,
-    categoryId: 2,
+    categoryId: 5, 
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=UU45SlFXcU5IaWw0OFdaTXJUOVFNYjc0eHpSdklzMFBxYVdxTE52V2Yzdz0
+
   },
   {
-    code: 'F0029',
-    name: 'Pantalla Reflectora 5 En 1 Circular 80 Cm Con Funda Rebote',
-    price: 125000,
-    stock: 2,
+    code: "SYM",
+    name: "Synco Kit de transmisión en vivo SY-M1P-Mic",
+    price: 4999,
+    stock: 6,
     description: ` 
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
     `,
 
-    image:
-      'https://http2.mlstatic.com/D_NQ_NP_843094-MLA41772018770_052020-O.webp',
+    image: "https://www.digi-zoom.com.ar/images/productos/2059_1_z.jpg",
     state: true,
-    categoryId: 1,
+    categoryId: 5, 
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=bUlkbk9wV1VEUVEzdWtRTG9rM2JBRDJoSmI0WTFpWkpYbkpyN2RCNlp0RT0
+
   },
+  {
+    code: "Gdox",
+    name: "Godox Micrófono de escopeta compacto VD-Mic",
+    price: 4999,
+    stock: 16,
+    description: `
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    `,
+
+    image: "https://www.digi-zoom.com.ar/images/productos/2109_1_z.jpg",
+    state: true,
+    categoryId: 5, 
+    // https://www.digi-zoom.com.ar/productos_detalle.php?i=RmFqTFBpMHk4RjM5WUNibzBKTkFaRVRHR3FZbVVaTlZpbWdWNkpVVnZlOD0
+  }
 ];
