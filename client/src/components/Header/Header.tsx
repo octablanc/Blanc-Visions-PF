111d6ff53856ab23133e9a7544ffd0db57e082a6
Tomas Flores <tomy24ssf@gmail.com>
2022-12-16 10:21:03 -0300

    adding buttons opcions

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import {
   Input,
   Menu,
   Nav,
+  Desplegable
 } from './styled-components/styles';
 import { HiOutlineMagnifyingGlass } from 'react-icons/hi2';
 import { FaUserCircle } from 'react-icons/fa';
@@ -12,18 +13,34 @@ import { BsFillCartFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import { getAllCategories, getProductsPage } from '../../redux/slices/products';
 import { useAppDispatch, useAppSelector } from '../../redux/app/hooks';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export const Header = () => {
   const { categories, pagination } = useAppSelector(
     (state) => state.productsState
   );
-  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
+  const dispatch = useAppDispatch();
+    const [selectActive, setSelectActive] = useState(false);
   const handleClick = (e: any): void => {
     const value = e.target.innerText;
     dispatch(getProductsPage(1, pagination.quantity, value));
   };
+  const handleHover = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (selectActive) {
+    return setSelectActive(false);      
+    }
+    setSelectActive(true);
+  }
+  const navigatePage = (e:any) => {
+    const { innerText } = e.target;
+    if (innerText !== 'perfil') return console.log(innerText);
+    navigate(`/profile`);
+    setSelectActive(false)
+  }
 
   useEffect(() => {
     dispatch(getAllCategories());
@@ -43,10 +60,17 @@ export const Header = () => {
             </form>
             <Icons>
               <li>
-                <Link to='/profile'>
+                <div onClick={handleHover}>
                   <FaUserCircle />
                   Mi Cuenta
-                </Link>
+                </div>
+                {selectActive &&
+                  <Desplegable>
+                    <li onClick={navigatePage}>perfil</li>
+                    <li onClick={navigatePage}>compras</li>
+                    <li onClick={navigatePage}>cerrar sesion</li>
+                  </Desplegable>
+                }
               </li>
 
               <li>
