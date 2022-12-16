3179c9128b25ae4df91876ae0fbc3b8a26c40f48
Tomas Flores <tomy24ssf@gmail.com>
2022-12-16 12:36:03 -0300

    ading properties

--- API/app/utils.ts
+++ API/app/utils.ts
@@ -81,20 +81,20 @@ export const data = [
     ],
     properties: [
       {
-        name: 'gola',
-        value: 'valor de esto',
+        name: 'clave',
+        value: 'valor del prop',
       },
       {
-        name: 'gola',
-        value: 'valor de esto',
+        name: 'clave',
+        value: 'valor del prop',
       },
       {
-        name: 'gola',
-        value: 'valor de esto',
+        name: 'clave',
+        value: 'valor del prop',
       },
       {
-        name: 'gola',
-        value: 'valor de esto',
+        name: 'clave',
+        value: 'valor del prop',
       },
     ]
   },
@@ -125,6 +125,24 @@ export const data = [
           "https://http2.mlstatic.com/D_NQ_NP_607595-MLA48551234827_122021-O.webp",
       }
     ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+    ]
   },
   {
     code: "Peak",
@@ -148,6 +166,24 @@ export const data = [
           "https://www.digi-zoom.com.ar/images/productos/2182_2_z.jpg",
       },
     ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+    ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=aXU5dlNlem5lU2VtckIvSFNHdktUaUhabFMvMW9pY1BUQ1MyWnBvVGJwYz0
   },
   {
@@ -171,6 +207,24 @@ export const data = [
       {
         url_image: 'https://www.digi-zoom.com.ar/images/productos/1751_4_z.jpg',
       }
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     //https://www.digi-zoom.com.ar/productos_detalle.php?i=VU5vRGNYVEpOQmZZellSb011d3pPNlluMFdSRURpNVNFNEVxTDZLVVc3cz0
   },
@@ -190,6 +244,24 @@ export const data = [
       { url_image: 'https://www.digi-zoom.com.ar/images/productos/1488_1_z.jpg' },
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/1488_3_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/1488_6_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
   // https://www.digi-zoom.com.ar/productos_detalle.php?i=NjR3dGRIZTY1K05welpBSWhUS1RQNWdPWmZtd0ZObldIVkR0ckdkSU9kRT0
   },
@@ -209,6 +281,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/1489_1_z.jpg'},
       { url_image: 'https://www.digi-zoom.com.ar/images/productos/1489_6_z.jpg' },
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/1489_3_z.jpg'}
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     //https://www.digi-zoom.com.ar/productos_detalle.php?i=Wlp6T0pqT04xN1B0bWd4SThESlZGZVVXT2VuWmpsQ1ViMnliRXFuZXNtMD0
   },
@@ -228,6 +318,24 @@ export const data = [
       { url_image: 'https://www.digi-zoom.com.ar/images/productos/300_1_z.jpg' },
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/300_3_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/300_2_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=N2dkSzVpWU5halo0RmJrNmFhUzdDRmdEMFZwRmtIWXNqc0kzRUNRTGRhcz0
   },
@@ -246,6 +354,24 @@ export const data = [
     images: [
       { url_image: 'https://www.digi-zoom.com.ar/images/productos/2283_1_z.jpg' },
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2283_2_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=cFZyb0lGbnUrNTA0MHFEVUo5TjZlTFFFTGNtQ2dWN3EyTk41MW5NM1B3MD0
   },
@@ -265,6 +391,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/1916_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/1916_3_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/1916_8_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=WDhjSUo2UzNiMTZzSUFDMy8zYkw2aGQ3amJnWHh1a3R4d2EvYUl5ZVpPaz0
   },
@@ -283,6 +427,24 @@ export const data = [
     images: [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/640_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/640_2_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=NyttZzZYdFNTNnNVdXZXbXIzMFJLVWxQczVCQzF4SDNjbzVlc3FGM0FITT0
   },
@@ -302,6 +464,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2181_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2181_3_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2181_6_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=aEY5Z2RrZE84emtZK1NZckt3azZlTUpqNmJhRnE3VmRudHJTUmF2Q2dMOD0
   },
@@ -321,6 +501,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2197_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2197_2_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2197_6_z.jpg'}
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=V0cvcDg2MFNjbXBlZXNkck5VSjlSYUF1T1dmRmg5cEFaZUg0UVF3YXVVQT0
   },
@@ -339,6 +537,24 @@ export const data = [
       { url_image: 'https://www.digi-zoom.com.ar/images/productos/2197_1_z.jpg' },
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2197_2_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2197_5_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=V0cvcDg2MFNjbXBlZXNkck5VSjlSYUF1T1dmRmg5cEFaZUg0UVF3YXVVQT0
   },
@@ -357,6 +573,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2069_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2069_2_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2069_4_z.jpg'}
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=U0kwVTRQR2hXV2NQVWd5bDgvVlY5SHU3WVcxUkhCOTRnbThQd0dScWw1Yz0
   },
@@ -376,6 +610,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2066_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2066_2_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2066_4_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=UU45SlFXcU5IaWw0OFdaTXJUOVFNYjc0eHpSdklzMFBxYVdxTE52V2Yzdz0
 
@@ -396,6 +648,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2059_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2059_2_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2059_3_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=bUlkbk9wV1VEUVEzdWtRTG9rM2JBRDJoSmI0WTFpWkpYbkpyN2RCNlp0RT0
 
@@ -416,6 +686,24 @@ export const data = [
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2109_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2109_1_z.jpg'},
       {url_image: 'https://www.digi-zoom.com.ar/images/productos/2109_2_z.jpg'},
+    ],
+    properties: [
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
+      {
+        name: 'clave',
+        value: 'valor del prop',
+      },
     ]
     // https://www.digi-zoom.com.ar/productos_detalle.php?i=RmFqTFBpMHk4RjM5WUNibzBKTkFaRVRHR3FZbVVaTlZpbWdWNkpVVnZlOD0
   }
