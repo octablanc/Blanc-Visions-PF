c04cbb7963f48a7789ec86b587ec5bf31ac3996d
Tomas Flores <tomy24ssf@gmail.com>
2022-12-21 12:23:24 -0300

    adding get, post, set orderBuy

--- API/app/routes/order-buy.ts
+++ API/app/routes/order-buy.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { getOrderBuy } from "../controllers/orderBuyControllers";
+import { getCartUser, getOrderBuy, getOrderBuyUserId, postOrderBuy,setOrderBuy } from "../controllers/orderBuyControllers";
 
 const router = express.Router();
 
 router.get("/", getOrderBuy);
-
-
-// router.delete('/', postImages);
+router.get("/cart/:userId", getCartUser);
+router.get("/:userId", getOrderBuyUserId);
+router.post("/", postOrderBuy);
+router.put("/:id", setOrderBuy);
 
 module.exports = router;
