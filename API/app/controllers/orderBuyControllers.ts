7aa4e77e1cc166f5c0c3e3658f99f1467daf2eae
Tomas Flores <tomy24ssf@gmail.com>
2022-12-21 12:22:15 -0300

    ruta crud controllers

--- API/app/controllers/orderBuyControllers.ts
+++ API/app/controllers/orderBuyControllers.ts
@@ -29,6 +29,107 @@ export async function getOrderBuy(_req: Request, res: Response) {
     return res.status(400).send({ message });
   }
 }
+
+export async function getOrderBuyUserId(req: Request, res: Response) {
+  const { userId } = req.params;
+  try {
+    const ordenBuyUser = await orderBuy.findAll({
+      where:{userId,buy:true},
+      include: [
+        {
+          model: ProductOrder,
+          include: [{
+            model: Products,
+            attributes: ['id','name','image']
+          }],
+          attributes: ['quantity','price']
+        },
+      ],
+      attributes: ['id','priceTotalDiscount','buy']
+    });
+    
+    return res.json({ message: "ORDER BUY .",ordenBuyUser });
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(400).send({ message });
+  }
+}
+
+export async function getCartUser(req: Request, res: Response) {
+  const { userId } = req.params;
+  try {
+    const ordenBuyUser = await orderBuy.findAll({
+      where:{userId,buy:false},
+      include: [
+        {
+          model: ProductOrder,
+          include: [{
+            model: Products,
+            attributes: ['id','name','image']
+          }],
+          attributes: ['quantity','price']
+        },
+      ],
+      attributes: ['id','priceTotalDiscount','buy']
+    });
+    
+    return res.json({ message: "ORDER BUY .",ordenBuyUser });
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(400).send({ message });
+  }
+}
+
+export async function setOrderBuy(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const orderBuyId = await orderBuy.findByPk(id);
+
+    await orderBuyId?.update({ buy: true });
+    await orderBuyId?.save();
+
+
+    return res.json({ message: "ORDER BUY ." });
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(400).send({ message });
+  }
+}
+
+
+export async function postOrderBuy(req: Request, res: Response) {
+  const { priceTotalDiscount, discount, state, postalCode, street, height, city, dues, userId, buy, productOrders } = req.body;
+  try {
+    
+    const orderBuyCreate = await orderBuy.create({priceTotalDiscount, discount,state,postalCode,street,height,city,dues,userId,buy,productOrders},
+      { include: [{ model: ProductOrder }, { model: Products },] });
+    return res.json({ message: "ORDER BUY .",orderBuyCreate });
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(400).send({ message });
+  }
+}
+// {
+//   "priceTotalDiscount":12390321,
+//   "discount": 20123,
+//   "state": true,
+//   "postalCode": 112399,
+//   "street": "otra",
+//   "height": "2318493",
+//   "city": "bninon",
+//   "dues": 130432432,
+//   "userId": 2,
+//   "buy": false,
+//   "productOrders": [
+//     {
+//       "productId": 2,
+//       "quantity": 12,
+//       "price": 1000
+//     }
+//   ]
+// }
+
+
 // User.findAll({
 //   include: [
 //     {
@@ -47,22 +148,3 @@ export async function getOrderBuy(_req: Request, res: Response) {
 //     }
 //   ]
 // });
-
-
-
-// {
-//   calle: 'jfdksaf'
-//  descento: 1203,
-//   products: [
-//     {
-//       userId: 1
-//       productId: 1,
-//       quantity: 19,
-//     },
-//     {
-//       userId: 1
-//       productId: 2,
-//       quantity: 19,
-//     }
-//   ]
-// }
