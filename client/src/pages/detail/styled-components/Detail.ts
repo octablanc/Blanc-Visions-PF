8d626fe9333304f42987ddc5b55b1d55f4122efd
Noelia Ferrer <noelia.c.ferrer@gmail.com>
2022-12-22 20:18:49 -0300

    global-fonts

--- client/src/pages/detail/styled-components/Detail.ts
+++ client/src/pages/detail/styled-components/Detail.ts
@@ -22,25 +22,11 @@ export const Image = styled.div`
   }
 `;
 
-// export const Title = styled.div`
-//   background: yellow;
-//   color: #c2ad94;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-around;
-//   align-items: center;
-//  `;
-
 export const Info = styled.div`
   /* background-color: red; */
   display: flex;
   flex-direction: column;
   margin: 6rem 0rem 0rem 0rem;
-  h3 {
-    font-size: 2.5rem;
-    font-weight: normal;
-    color: black;
-  }
   .icons {
     display: flex;
     justify-content: space-between;
@@ -66,29 +52,6 @@ export const CartSection = styled.div`
   align-items: center;
 `;
 
-// export const Counter = styled.div`
-//   /* background: yellow; */
-//   border-color: #c2ad94;
-//   margin-top: -2rem;
-//   width: 10rem;
-//   height: 5rem;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-around;
-//   align-items: center;
-//   div {
-//     font-size: 2rem;
-//   }
-//   button {
-//     border: none;
-//     color: black;
-//     font-size: 2rem;
-//     font-weight: bold;
-//     background: transparent;
-//     cursor: pointer;
-//   }
-// `;
-
 export const Btn = styled.button`
   width: 70%;
   font-size: 1.6rem;
@@ -98,6 +61,5 @@ export const Btn = styled.button`
   background: black;
   color: white;
   border: none;
-  /* box-shadow: inset 1rem 1rem 2rem #a1907b, inset -1rem -1rem 2rem #e3caad; */
   cursor: pointer;
 `;
--- client/src/pages/detail/styled-components/Slider/Slider.ts
+++ client/src/pages/detail/styled-components/Slider/Slider.ts
@@ -10,15 +10,6 @@ export const Container = styled.div`
   box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.2);
 `;
 
-// export const ContainerImg = styled.div`
-//   background: blue;
-//   width: 100%;
-//   height: 100%;
-//   position: absolute;
-//   opacity: 1;
-//   transition: opacity ease-in-out 0.4s;
-// `;
-
 export const Img = styled.img`
   width: 100%;
   height: 100%;
@@ -31,7 +22,6 @@ export const Thumbnails = styled.div`
   height: 10rem;
   display: flex;
   flex-direction: row;
-
 `;
 
 export const Miniatures = styled.div`
