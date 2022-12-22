0de565283ee9dfeb040089c0397b4f2ebb7515a9
Agustina Zanetti <agus.zanetti4@gmail.com>
2022-12-22 11:27:24 -0300

    update about-link in footer

--- client/src/pages/about/about.tsx
+++ client/src/pages/about/about.tsx
@@ -1,30 +1,54 @@
 import { Columns, Box } from './styled-components/about';
-import { BsFillEnvelopeOpenFill, BsFillTelephoneFill } from '../../icons/index';
+import {
+  BsFillEnvelopeOpenFill,
+  BsWhatsapp,
+  BsFillTelephoneFill,
+} from '../../icons/index';
+import { Sales } from '../home/components/Sales/Sales';
 
 export const About = () => {
   return (
     <div className='container'>
       <Columns>
         <Box>
-          <h3>About us</h3>
+          <h3>Nosotros</h3>
           <p>
-            Somos una empresa dedicada a la venta de productos de calidad, con
-            una amplia gama de productos para todos los gustos y necesidades del
-            cliente.
+            Kingcomm tiene sus inicios en 2022, enfocado en brindar a sus
+            clientes los mejores equipos y accesorios siempre de las mejores
+            marcas, para brindarles la calidad y seguridad que merecen.
+          </p>
+          <p>
+            Somos una tienda online abierta las 24hs los 365 días del año,
+            especializada en fotografía, video e iluminación para el aficionado
+            y el profesional.
+          </p>
+          <p>
+            Detrás de la innovación y las nuevas necesidades, buscamos ofrecer
+            las últimas tecnologías disponibles del mercado.
+          </p>
+          <p>
+            Para una atención más personalizada podes contactarte vía whatsapp
+            de lunes a viernes de 9 a 18hs.
           </p>
         </Box>
         <Box>
-          <h3>Contact us</h3>
+          <h3>Contáctanos</h3>
           <p>
-            <BsFillEnvelopeOpenFill style={{ marginRight: '2rem' }} />
+            <BsFillEnvelopeOpenFill className='icons' />
             <span>info@kingcomm.com</span>
           </p>
           <p>
-            <BsFillTelephoneFill style={{ marginRight: '2rem' }} />
+            <BsWhatsapp className='icons' />
             <span>+54 1121838240</span>
+          </p>
+          <p>
+            <BsFillTelephoneFill className='icons' />
+            <span>(011) 2100 6019</span>
           </p>
         </Box>
       </Columns>
+      <h4>Aprovecha estos descuentos</h4>
+      <Sales />
     </div>
   );
 };
