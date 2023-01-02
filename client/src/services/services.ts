ab9dedabea6f0d3e8cd4ba1966fc539fed1b9179
Noelia Ferrer <noelia.c.ferrer@gmail.com>
2023-01-02 11:46:41 -0300

    rompela Flor!!

--- client/src/services/services.ts
+++ client/src/services/services.ts
@@ -1,5 +1,25 @@
 import axios from "axios";
 
+export interface ProductOrderIfc {
+    productId: number
+    quantity: number
+    price: number
+  }
+  
+  export interface OrderBuyIfc {
+    priceTotalDiscount: number
+    discount: number
+    state: boolean
+    postalCode: number
+    street: string
+    height: string
+    city: string
+    dues: number
+    buy: boolean
+    userId: number
+    productOrders: Array<ProductOrderIfc>
+  }
+
 export async function postUser(user: object){
     try {
         await axios.post('http://localhost:3001/users', user);
@@ -15,3 +35,12 @@ export async function updateUser(user: object, id: Number | undefined) {
         console.log(message);
     }
 }
+
+export async function postOrderBuy(orderBuy: Object) {
+    try {
+        await axios.post('http://localhost:3001/order-buy', orderBuy);
+    } catch ({message}) {
+        console.log(message);
+    }
+}
+
