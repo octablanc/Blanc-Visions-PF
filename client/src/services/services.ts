35d2702f5a07b57f9d11337b3711ff853d5938af
Tomas Flores <tomy24ssf@gmail.com>
2023-01-03 19:58:35 -0300

    agregando el rating en la orden de compra

--- client/src/services/services.ts
+++ client/src/services/services.ts
@@ -1,46 +1,72 @@
-import axios from "axios";
+import axios from 'axios';
 
 export interface ProductOrderIfc {
-    productId: number
-    quantity: number
-    price: number
-  }
-  
-  export interface OrderBuyIfc {
-    priceTotalDiscount: number
-    discount: number
-    state: boolean
-    postalCode: number
-    street: string
-    height: string
-    city: string
-    dues: number
-    buy: boolean
-    userId: number
-    productOrders: Array<ProductOrderIfc>
-  }
+  productId: number;
+  quantity: number;
+  price: number;
+}
 
-export async function postUser(user: object){
-    try {
-        await axios.post(`${process.env.REACT_APP_BACKEND_URL}/users`, user);
-    } catch (error) {
-        console.log(error);
-    }
+export interface OrderBuyIfc {
+  priceTotalDiscount: number;
+  discount: number;
+  state: boolean;
+  postalCode: number;
+  street: string;
+  height: string;
+  city: string;
+  dues: number;
+  buy: boolean;
+  userId: number;
+  productOrders: Array<ProductOrderIfc>;
+}
+
+interface RatingIfc {
+  commentary: string;
+  score: number;
+  productId: number;
+}
+
+export async function postUser(user: object) {
+  try {
+    await axios.post(`${process.env.REACT_APP_BACKEND_URL}/users`, user);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function updateUser(user: object, id: Number | undefined) {
-    try {
-        await axios.put(`${process.env.REACT_APP_BACKEND_URL}/users/` + id, user);
-    } catch ({message}) {
-        console.log(message);
-    }
+  try {
+    await axios.put(`${process.env.REACT_APP_BACKEND_URL}/users/` + id, user);
+  } catch ({ message }) {
+    console.log(message);
+  }
 }
 
 export async function postOrderBuy(orderBuy: Object) {
-    try {
-        await axios.post(`${process.env.REACT_APP_BACKEND_URL}/order-buy`, orderBuy);
-    } catch ({message}) {
-        console.log(message);
-    }
+  try {
+    await axios.post(
+      `${process.env.REACT_APP_BACKEND_URL}/order-buy`,
+      orderBuy
+    );
+  } catch ({ message }) {
+    console.log(message);
+  }
 }
 
+export const postRating = async ({
+  score,
+  commentary,
+  productId,
+}: RatingIfc) => {
+  try {
+    // const ratingCreate = (
+      await axios.post(`${process.env.REACT_APP_BACKEND_URL}/ratings`, {
+        score,
+        commentary,
+        productId,
+      })
+    // ).data;
+  } catch ({ message }) {
+    console.log(message);
+  }
+};
